541728014cf40ed8de83a31add009dce7fc79c76
Jack Phillips <Jhphillips1994@gmail.com>
2018-09-15 23:15:59 +1000

    Starting adding mylocation functionality

--- app/src/main/java/com/photoscape/photoscape/MapsActivity.java
+++ app/src/main/java/com/photoscape/photoscape/MapsActivity.java
@@ -1,11 +1,18 @@
 package com.photoscape.photoscape;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
@@ -25,7 +32,7 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
     private FirebaseAuth authCheck;
@@ -45,7 +52,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //authCheck = FirebaseAuth.getInstance();
         authCheck = FirebaseAuth.getInstance();
         FirebaseUser currentUser = authCheck.getCurrentUser();
-        if (authCheck.getCurrentUser() == null){
+        if (authCheck.getCurrentUser() == null) {
             authenticateUser();
         }
 
@@ -62,6 +69,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 signOut();
             }
         });
+
+        // Setting up the mylocation
+        // Checking that the application has the correct permissions to access the device's location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
     }
 
 
@@ -82,6 +104,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Setup mylocation button
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
     }
 
     // Authenticate the user by calling the Firebase intent
@@ -123,9 +149,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AuthUI.getInstance()
                 .signOut(this)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    public void onComplete(@NonNull Task<Void> task) {
+                    public void onComplete(@NonNull Task<Void> task) {}});
+    }
 
-                    }
-                });
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    // Handling the button that gets the user's current location
+    private void usersCurrentLocation() {
+        FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.myLocationButton);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
