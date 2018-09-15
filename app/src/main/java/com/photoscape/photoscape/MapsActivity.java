6af60ad47450cf0210b06edc7e3c37692a98c141
Jack Phillips <Jhphillips1994@gmail.com>
2018-09-15 21:15:00 +1000

    Added Sign in functionality and sign out button

--- app/src/main/java/com/photoscape/photoscape/MapsActivity.java
+++ app/src/main/java/com/photoscape/photoscape/MapsActivity.java
@@ -1,27 +1,67 @@
 package com.photoscape.photoscape;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseAuth authCheck;
+    private static int REQUEST_CODE = 102;
+
+    // Setting up Firebase login providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Check if a user is already logged in
+        //authCheck = FirebaseAuth.getInstance();
+        authCheck = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = authCheck.getCurrentUser();
+        if (authCheck.getCurrentUser() == null){
+            authenticateUser();
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Handling the sign out button
+        Button signOutButton = (Button) findViewById(R.id.signOutButton1);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Sign user out
+                signOut();
+            }
+        });
     }
 
 
@@ -42,5 +82,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    // Authenticate the user by calling the Firebase intent
+    private void authenticateUser() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),REQUEST_CODE);
+    }
+
+    // Handle the return value from the AuthUI intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Logged in
+            }
+            else if (response == null) {
+                // user cancelled signin
+
+            }
+            else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                // Device had no network connection
+                return;
+            }
+            else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                // Unknown error occured
+                return;
+            }
+        }
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
     }
 }
